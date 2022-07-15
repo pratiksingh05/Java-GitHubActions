74379d608336e65d3a48bc005ca0ab7a618ffe63
Pratiksingh05 <pratikmotihari@gmail.com>
2022-07-15 20:15:38 +0530

    s3 code

--- scripts/aws-test.sh
+++ scripts/aws-test.sh
@@ -16,6 +16,7 @@ echo "installed aws"
 # Configure AWS CLI
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region us-west-1
 
 aws s3 ls
 
