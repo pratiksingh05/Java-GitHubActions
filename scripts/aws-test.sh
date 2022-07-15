83d2a85f91d01f36d45d41b4807bf4dcbee3a907
Pratiksingh05 <pratikmotihari@gmail.com>
2022-07-15 19:42:16 +0530

    updated code for sh file

--- scripts/aws-test.sh
+++ scripts/aws-test.sh
@@ -1,4 +1,3 @@
-
 $AWS_ACCESS_KEY_ID=$1
 $AWS_SECRET_ACCESS_KEY=$2
 $AWS_BUCKET=$3
@@ -8,8 +7,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscl
 unzip awscliv2.zip
 chmod +x ./aws/install
 ./aws/install
-aws --version
+#aws --version
 
+echo "installed aws"
 # Configure AWS CLI
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
