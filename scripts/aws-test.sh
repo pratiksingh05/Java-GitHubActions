4f7676fc0e9ea0aa24a8d0f0e6416ba8fb2828e3
Pratiksingh05 <pratikmotihari@gmail.com>
2022-07-15 20:05:23 +0530

    s3 code update

--- scripts/aws-test.sh
+++ scripts/aws-test.sh
@@ -2,18 +2,23 @@ AWS_ACCESS_KEY_ID=$1
 AWS_SECRET_ACCESS_KEY=$2
 AWS_BUCKET=$3
 
+echo "AWS Bucket : $AWS_BUCKET"
+
 #install aws-cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 chmod +x ./aws/install
 ./aws/install
-#aws --version
+
+aws --version
 
 echo "installed aws"
 # Configure AWS CLI
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
+aws s3 ls
+
 echo "uploading... file to s3"
-aws s3 cp ./scripts/test.txt s3://$AWS_BUCKET/Unsaved/
+aws s3 cp ./upload-file/test.txt s3://$AWS_BUCKET/Unsaved/
 echo "uploaded file to s3"
